20458e91cd07d34a0a1746f132e8161ddf770266
Denis Zykov <denis.zykov.19932@yandex.ru>
2022-12-01 11:38:37 +0300

    fixed: changed on demand

--- scripts/index.js
+++ scripts/index.js
@@ -5,13 +5,22 @@
 
 // эти переменные находят классы в html, чтобы управлять ими
 const popup = document.querySelector('.popup');
-const editButton = document.querySelector('.profile__edit-button').addEventListener('click', openPopup);
+const editButton = document.querySelector('.profile__edit-button');
 // эта переменная работает только через переменную "popup", через документ ломается функция
-const closeButton = popup.querySelector('.popup__close-button').addEventListener('click', closePopup);
+const closeButton = popup.querySelector('.popup__close-button');
 
-// это слушатели событий. я их сразу добавил в переменную
-// editButton.addEventListener('click', openPopup);
-// closeButton.addEventListener('click', closePopup);
+// Редактирование имени и информации о себе
+
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__form');
+
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
+
+let nameTitle = document.querySelector('.profile__title');
+let jobSubtitle = document.querySelector('.profile__subtitle');
+
 
 // это функция открывает "popup" через замену "display: none" на "display: flex;" в "css"
 function openPopup() {
@@ -27,31 +36,16 @@ function closePopup() {
 
 // эти строчки кода закрывает "popup" по нажатию на область вне "popup"
 // желательно делать в связке так как может работать не предсказуемо
-//////////////////////////////
-popup.addEventListener('click', function(event){
-  if(!event.defaultPrevented){
-    closePopup();
-  }
-})
-document.querySelector('.popup__container').addEventListener('click', function(event){
-  event.preventDefault();
-})
+// //////////////////////////////
+// popup.addEventListener('click', function(event){
+//   if(!event.defaultPrevented){
+//     closePopup();
+//   }
+// })
+// document.querySelector('.popup__container').addEventListener('click', function(event){
+//   event.preventDefault();
+// })
 ///////////////////////////////
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Редактирование имени и информации о себе
-
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form');
-
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_job');
-
-let nameTitle = document.querySelector('.profile__title');
-let jobSubtitle = document.querySelector('.profile__subtitle');
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -70,3 +64,8 @@ function handleFormSubmit (evt) {
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// это слушатели событий
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
