3a4656c1ebf3590a2fe73bf2c5d1ea8864cad983
Denis Zykov <denis.zykov.19932@yandex.ru>
2022-12-14 12:58:05 +0300

    feat: code refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -23,19 +23,11 @@ const nameTitle = document.querySelector('.profile__title');
 const jobSubtitle = document.querySelector('.profile__subtitle');
 
 
-// это функция открывает "popup"
-function openPopup(popup) { popup.classList.add('popup_opened');}
+function openPopup(popup) { popup.classList.add('popup_opened');} // эта функция открывает "popup"
+function closePopup(popup) { popup.classList.remove('popup_opened');} // эта функция закрывает "popup"
+function changeLike(evt){ evt.target.classList.toggle('element__like-button_active');} // эта функция изменяет состояние кнопки лайк
 
-// это функция закрывает "popup". так же меняет "display"
-function closePopup(popup) { popup.classList.remove('popup_opened');}
-
-// эта функция изменяет состояние кнопки лайк
-function changeLike(evt){
-  evt.target.classList.toggle('element__like-button_active');
-}
-////////////////////////////////////////////////////////////////////////////////////////////////////////
 // это слушатели событий
-
 editForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   nameTitle.textContent = nameInput.value;
@@ -52,12 +44,11 @@ editButton.addEventListener('click', () => {
 closeButton.addEventListener('click', ()=> closePopup(popupEditProfile)); // этот слушатель закрывает Попап
 likeButton.addEventListener('click', changeLike); // этот слушатель изменяет лайк
 
-addButton.addEventListener('click', openPopup);
+addButton.addEventListener('click', ()=> {openPopup(popupNewCard)});
 
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 // эти строчки кода закрывает "popup" по нажатию на область вне "popup"
 // желательно делать в связке так как может работать не предсказуемо
-// //////////////////////////////
 // popup.addEventListener('click', function(event){
 //   if(!event.defaultPrevented){
 //     closePopup();
@@ -67,9 +58,7 @@ addButton.addEventListener('click', openPopup);
 //   event.preventDefault();
 // })
 ///////////////////////////////
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+// Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
 // function handleFormSubmit(evt) {
 //   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 //   nameTitle.textContent = nameInput.value;// Находим "profile__title", чтобы вставить имя с помощью "value"
