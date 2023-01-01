edd178248bdff281eafa0747c9886ccf7b7406ef
Denis Zykov <denis.zykov.19932@yandex.ru>
2023-01-01 12:29:31 +0300

    style: add modifier and stile for popup save

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,8 +1,25 @@
-const popupBlock = document.querySelectorAll('.popup');
+const formEditForm = document.forms.editForm;
+const nameInputForm = formEditForm.elements.nameInput;
+const jobInputForm = formEditForm.elements.jobInput;
+
+function setSubmitButtonState(isFormValid){
+  if (isFormValid) {
+    addButton.removeAttribute('disabled');
+    addButton.classList.remove('popup__save_disabled');
+  } else {
+    addButton.setAttribute('disabled', true);
+    addButton.classList.add('popup__save_disabled');
+  }
+}
+
+formEditForm.addEventListener('input', function (evt) {
+  const isValid = nameInputForm.value.length > 0 && jobInputForm.value.length > 0;
+  setSubmitButtonState(isValid);
+});
 
 
 
 
 // включение валидации вызовом enableValidation
 // все настройки передаются при вызове
-// enableValidation();
+enableValidation(validationConfig)
