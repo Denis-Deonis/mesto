9a813a1c3be1be84fe433f6f53fa5662858dddf8
Denis Zykov <denis.zykov.19932@yandex.ru>
2023-01-12 16:38:37 +0300

    fixed: delete file

--- scripts/validationConfig.js
+++
@@ -1,9 +0,0 @@
-const validationConfig = {
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__save',
-  inactiveButtonClass: 'popup__save_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'error'
-}
-
