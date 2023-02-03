3605cfef168366df4418522d7e494b2539481367
Denis Zykov <denis.zykov.19932@yandex.ru>
2023-02-03 12:26:18 +0300

    feat: creat new class Section

---
+++ src/components/Section.js
@@ -0,0 +1,24 @@
+export class Section {
+  constructor({items, renderer}, selectorContainer) {
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  addItem(element){
+    this._container.append(element);
+  }
+
+  _clear(){
+    this._container.innerHTML = '';
+  }
+
+  renderItems() {
+    this._clear();
+
+    this._items.forEach((item) => {
+      this._renderer(item);
+    });
+  }
+
+}
